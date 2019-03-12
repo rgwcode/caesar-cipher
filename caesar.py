21e52c89bb2a659c3d1c0ee60db3be5f0ffd4ac6
Roby Weidig <roby@weidig.lu>
2019-03-12 13:24:07 -0500

    Broke up into functions.

--- caesar.py
+++ caesar.py
@@ -1,29 +1,59 @@
 #!/usr/bin/env python3
 MAX_KEY     =  26
 
+def operation(instruction):
+   return input(instruction).lower()
+
+def encrypt(text, key):
+    encrypted_text = ""
+    for character in text:
+        if character.isalpha():
+            encrypted_character = (chr(ord(character) + key))
+            # handle overflowing for upper and lower
+            if character.isupper():
+                if ord(encrypted_character) > ord("Z"):
+                    encrypted_character = (chr(ord(encrypted_character) - MAX_KEY))
+            else:
+                if ord(encrypted_character) > ord("z"):
+                    encrypted_character = (chr(ord(encrypted_character) - MAX_KEY))
+            encrypted_text = encrypted_text + encrypted_character
+        else:
+            encrypted_text = encrypted_text + character
+    return encrypted_text
+
+def getInputText(file = ""):
+    with open(filename, 'r') as f:
+        text = f.read()
+    return text
+
+def outputText(encrypted_text):
+    print("The encrypted text is:\n" + encrypted_text)
+
+def decrypt(text, key):
+    pass
+
 print("-Caesar Cipher Testprogram-")
 print("-Choose the operation to be performed-")
-op = input("Type e for encrypting a file, d for decrypting ").lower()
+
+op = ''
+while not(op == 'e' or op == 'd'):
+    print(op)
+    op = operation("Type e for encrypting a file, d for decrypting ")
 
 if op == 'e':
     print("Encrypting.")
     key = int(input("Type in key: "))
-    filename = input("Type the path to a text file to encrypt: ")
 
-    with open(filename, "r") as f:
-        wholetext = f.read()
+    whole_text = ""
+    while whole_text == "":
+        try:
+            filename = input("Type the path to a text file to encrypt: ")
+            whole_text = getInputText(filename)
+        except:
+            print("That file does not exist. Please try again.")
 
-    encryptedtext = ""
-    for character in wholetext:
-        if character.isalpha():
-            encryptedcharacter = (chr(ord(character) + key))
-            if character.isupper():
-                if ord(encryptedcharacter) > ord("Z"):
-                    encryptedcharacter = (chr(ord(encryptedcharacter) - MAX_KEY))
-            else:
-                if ord(encryptedcharacter) > ord("z"):
-                    encryptedcharacter = (chr(ord(encryptedcharacter) - MAX_KEY))
-            encryptedtext = encryptedtext + encryptedcharacter
-        else:
-            encryptedtext = encryptedtext + character
-    print(encryptedtext)
+    encrypted_text = encrypt(whole_text, key)
+    outputText(encrypted_text)
+
+elif op == 'd':
+    print("Not implemented yet.")
